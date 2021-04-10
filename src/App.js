3fe23beafad16b9326cc3c78305617e774e8b19f
Mike <kolodziejczykmichal269@gmail.com>
2021-04-10 20:32:49 +0200

    Create UserPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ForgotNewPasswordPage from "./pages/forgot-new-password/forgot-new-password.component";
 import ConfirmSignUpPage from "./pages/confirm-sign-up/confirm-sign-up.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import UserPage from "./pages/user-page/user-page.component";
 import "./App.css";
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
           path="/forgot-password"
           component={ForgotNewPasswordPage}
         />
+        <Route exact path="/user" component={UserPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/user-page/user-page.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./user-page.styles.scss";
+
+const UserPage = () => {
+  return (
+    <div className="user-page">
+      <h1>User Page</h1>
+    </div>
+  );
+};
+
+export default UserPage;
