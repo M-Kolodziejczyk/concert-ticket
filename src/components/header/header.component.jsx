adec2ba3d4e1883524412e9b2241feabbff95078
Mike <kolodziejczykmichal269@gmail.com>
2021-04-19 21:36:58 +0200

    Acc condition rendering in Header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,32 +1,51 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { signOutStart } from "../../redux/user/user.actions";
 
 import { ReactComponent as Logo } from "../../assets/concert.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
-  <div className="header">
-    <Link className="logo__container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options__container">
-      <Link to="/" className="options__link">
-        Home
+const Header = () => {
+  const dispatch = useDispatch();
+  const isLogged = useSelector((state) => state.user.isLogged);
+
+  const handleSignOut = () => {
+    dispatch(signOutStart());
+  };
+
+  return (
+    <div className="header">
+      <Link className="logo__container" to="/">
+        <Logo className="logo" />
       </Link>
-      <Link to="/concerts" className="options__link">
-        Concerts
-      </Link>
-      <Link to="/user" className="options__link">
-        User
-      </Link>
-    </div>
-    <div className="auth__container">
-      <div className="auth__link">
-        <Link to="/signin">Signin</Link>
+      <div className="options__container">
+        <Link to="/" className="options__link">
+          Home
+        </Link>
+        <Link to="/concerts" className="options__link">
+          Concerts
+        </Link>
+      </div>
+      <div className="auth__container">
+        {isLogged ? (
+          <div className="user">
+            <Link to="/user" className="user__link">
+              Account
+            </Link>
+            <button onClick={handleSignOut} className="sign-out">
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <Link to="/signin" className="sign-in">
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
