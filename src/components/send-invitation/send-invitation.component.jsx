53c6fe985caa1ae2a3a50745ed265cab31b31604
MK <kolodziejczykmichal269@gmail.com>
2021-05-19 16:41:12 +0200

    Rewrite send-invite component

--- src/components/send-invitation/send-invitation.component.jsx
+++ src/components/send-invitation/send-invitation.component.jsx
@@ -1,28 +1,74 @@
-import React from "react";
-import useForm from "../../hooks/useForm";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
+import ErrorMessage from "../error-message/error-message.component";
 
-const SendInviation = ({
-  callback,
-  validate,
-  invitationID,
-  invitationTable,
-  authorEmail,
-}) => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    {
-      email: "",
-      message: "",
-      status: "new",
-      invitationID,
-      invitationTable,
-      authorEmail,
-    },
+const SendInviation = (props) => {
+  const dispatch = useDispatch();
+
+  const {
+    callback,
     validate,
-    callback
-  );
+    invitationID,
+    invitationTable,
+    authorEmail,
+    currentInvitations,
+  } = props;
+
+  const initialState = {
+    email: "",
+    message: "",
+    status: "new",
+    invitationID,
+    invitationTable,
+    authorEmail,
+  };
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [formError, setFormError] = useState("");
+  const invitations = [...currentInvitations];
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting && !formError) {
+      invitations.push(JSON.stringify({ email: values.email, status: "new" }));
+      dispatch(callback(values, invitations));
+      setValues(initialState);
+    }
+
+    //eslint-disable-next-line
+  }, [errors, isSubmitting, formError, dispatch]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values));
+    setFormError(validateForm(currentInvitations, values));
+
+    setIsSubmitting(true);
+  };
+
+  const validateForm = (currentInvitations, values) => {
+    let errors = "";
+    if (currentInvitations) {
+      currentInvitations.forEach((invitation) => {
+        if (values.email === JSON.parse(invitation).email) {
+          errors = `You already sent an invitation to ${values.email}`;
+        }
+      });
+    }
+
+    return errors;
+  };
 
   return (
     <div>
@@ -47,6 +93,7 @@ const SendInviation = ({
         <CustomButton type="submit" name="submit">
           Send
         </CustomButton>
+        {formError && <ErrorMessage>{formError}</ErrorMessage>}
       </form>
     </div>
   );
