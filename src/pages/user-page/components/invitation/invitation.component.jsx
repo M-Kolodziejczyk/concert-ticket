cc9a9c008c697cd4c1a753be9127f40835ea4d95
MK <kolodziejczykmichal269@gmail.com>
2021-05-20 13:23:39 +0200

    dispatch action listUserInvitationStart in invitation-tab

--- src/pages/user-page/components/invitation/invitation.component.jsx
+++ src/pages/user-page/components/invitation/invitation.component.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { listUserInvitationsStart } from "../../../../redux/invitation/invitation.actions";
 
 import "./invitation.styles.scss";
 
 const Invitation = ({ email }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (email) {
+      dispatch(listUserInvitationsStart(email));
+    }
+  }, [dispatch, email]);
+
   return (
     <div>
       <h1>Invitation Tab</h1>
