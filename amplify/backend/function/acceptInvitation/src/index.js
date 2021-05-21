40fbdaadfb2839bcfb0abb68a5c2769a9b827eb6
MK <kolodziejczykmichal269@gmail.com>
2021-05-21 19:55:48 +0200

    Create lambda function

---
+++ amplify/backend/function/acceptInvitation/src/index.js
@@ -0,0 +1,41 @@
+/* Amplify Params - DO NOT EDIT
+	API_CONCERTTICKET_ARTISTBANDJOINTABLE_ARN
+	API_CONCERTTICKET_ARTISTBANDJOINTABLE_NAME
+	API_CONCERTTICKET_BANDTABLE_ARN
+	API_CONCERTTICKET_BANDTABLE_NAME
+	API_CONCERTTICKET_GRAPHQLAPIIDOUTPUT
+	API_CONCERTTICKET_INVITATIONTABLE_ARN
+	API_CONCERTTICKET_INVITATIONTABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+async function createArtistBandJoin(params) {
+  try {
+    await docClient.put(params).promise();
+  } catch (error) {
+    return error;
+  }
+}
+
+exports.handler = async (event, context) => {
+  const dateObj = new Date();
+  const params = {
+    TableName: process.env.API_CONCERTTICKET_ARTISTBANDJOINTABLE_NAME,
+    Item: {
+      id: context.awsRequestId,
+      bandID: event.arguments.bandID,
+      artistID: event.arguments.artistID,
+      createdAt: dateObj,
+    },
+  };
+  try {
+    const res = await createArtistBandJoin(params);
+    console.log("RES", res);
+    return { body: "SUCCESS" };
+  } catch (err) {
+    return { error: err };
+  }
+};
