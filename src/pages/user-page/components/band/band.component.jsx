cfce72b6941f9ffad47a6ca28ac51821d625a8ff
MK <kolodziejczykmichal269@gmail.com>
2021-06-02 17:52:57 +0200

    Update band component in UserPage

--- src/pages/user-page/components/band/band.component.jsx
+++ src/pages/user-page/components/band/band.component.jsx
@@ -1,117 +1,63 @@
 import React from "react";
 import {
-  createBandStart,
+  uploadBandImageStart,
   createInvitationStart,
 } from "../../../../redux/band/band.actions";
-import validate from "../../../../validators/band";
+
+import useFileForm from "../../../../hooks/useFileForm";
 import validateInvite from "../../../../validators/send-invite";
-import useForm from "../../../../hooks/useForm.js";
 
+import CustomInputButton from "../../../../components/custom-input-button/custom-input-button.component";
 import CustomButton from "../../../../components/custom-button/custom-button.component";
-import FormInput from "../../../../components/form-input/form-input.component";
 import SendInvitation from "../../../../components/send-invitation/send-invitation.component";
-// import ErrorMessage from "../../../../components/error-message/error-message.component";
-
 import "./band.styles.scss";
 
-const Band = ({ userId, bands }) => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    { name: "", genre: "", userID: userId },
-    validate,
-    createBandStart
-  );
-
+const Band = ({ band }) => {
+  console.log("Band: ", band);
+  const { handleChangeImage, handleSubmitImage, imageUrl, imageErrors } =
+    useFileForm(band.id, ["image/jpeg"], uploadBandImageStart);
   return (
     <div className="band">
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Create Band
-      </button>
-
-      <div className="band-container">
-        <h2>Bands Created By you</h2>
-        <div className="band-createdBands">
-          {bands &&
-            bands.items.map((band) => (
-              <div key={band.id}>
-                <p>{band.name}</p>
-                <p>{band.genre}</p>
-                <p>{typeof JSON.parse(band.invitations)}</p>
-                <SendInvitation
-                  authorEmail={band.userName}
-                  senderTableElementID={band.id}
-                  senderTableElementName={band.name}
-                  senderTable="band"
-                  currentInvitations={band?.invitations || "[]"}
-                  callback={createInvitationStart}
-                  validate={validateInvite}
-                />
-                {band?.invitations &&
-                  JSON.parse(band?.invitations).map((invitation, i) => (
-                    <div key={i}>
-                      <div>
-                        <p>Email: {invitation.email}</p>
-                        <p>Status: {invitation.status}</p>
-                      </div>
-                    </div>
-                  ))}
+      <div className="details">
+        <p>Name: {band.name}</p>
+        <p>Genre: {band.genre}</p>
+      </div>
+      <SendInvitation
+        authorEmail={band.userName}
+        senderTableElementID={band.id}
+        senderTableElementName={band.name}
+        senderTable="band"
+        currentInvitations={band?.invitations || "[]"}
+        callback={createInvitationStart}
+        validate={validateInvite}
+      />
+      <div className="invitation-container">
+        <p>Sent Invitations:</p>
+        {band?.invitations &&
+          JSON.parse(band?.invitations).map((invitation, i) => (
+            <div key={i}>
+              <div>
+                <p>Email: {invitation.email}</p>
+                <p>Status: {invitation.status}</p>
               </div>
-            ))}
-        </div>
-      </div>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Create Band{" "}
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
             </div>
-            <div className="modal-body">
-              <form onSubmit={handleSubmit}>
-                <FormInput
-                  name="name"
-                  type="text"
-                  label="Name"
-                  handleChange={handleChange}
-                  value={values.name}
-                  error={errors.name}
-                />
-                <FormInput
-                  name="genre"
-                  type="text"
-                  label="Genre"
-                  handleChange={handleChange}
-                  value={values.genre}
-                  error={errors.genre}
-                />
-                <div className="form__button">
-                  <CustomButton type="submit" name="submit">
-                    Create
-                  </CustomButton>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+          ))}
       </div>
+      <form className="upload-image" onSubmit={handleSubmitImage}>
+        <CustomInputButton
+          onChange={handleChangeImage}
+          label="Select image"
+          name={band.id}
+          id={band.id}
+        />
+        {imageUrl && <img src={imageUrl} alt="artist img" />}
+        <div className="form__button">
+          <CustomButton type="submit" name="submit">
+            Upload Image
+          </CustomButton>
+          {imageErrors && <div className="imageErrors">{imageErrors}</div>}
+        </div>
+      </form>
     </div>
   );
 };
