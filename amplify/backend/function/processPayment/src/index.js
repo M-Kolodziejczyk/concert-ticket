41dc94f48cadb9516b09f30f5609d80155cde97f
MK <kolodziejczykmichal269@gmail.com>
2021-06-13 19:25:54 +0200

    Create processPayment Lambda

---
+++ amplify/backend/function/processPayment/src/index.js
@@ -0,0 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	API_CONCERTTICKET_GRAPHQLAPIIDOUTPUT
+	API_CONCERTTICKET_ORDERTABLE_ARN
+	API_CONCERTTICKET_ORDERTABLE_NAME
+	API_CONCERTTICKET_TICKETORDERTABLE_ARN
+	API_CONCERTTICKET_TICKETORDERTABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
