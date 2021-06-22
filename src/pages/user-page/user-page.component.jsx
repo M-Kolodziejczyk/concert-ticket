0cfc4f5c5eafcbcd56a48e3819751aec1357c1df
MK <kolodziejczykmichal269@gmail.com>
2021-06-22 16:00:37 +0200

    Update UserPage and delete old components

--- src/pages/user-page/user-page.component.jsx
+++ src/pages/user-page/user-page.component.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from "react";
 import { Link, Switch, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getUserStart } from "../../redux/user/user.actions";
 
 import UserArtistPage from "../user-artist-page/user-artist.component";
 import UserBandsPage from "../user-bands-page/user-bands-page.component";
 import UserBandPage from "../user-band-page/user-band-page.component";
-import InvitationsTab from "./components/invitations-tab/invitations-tab.component";
 import UserConcertsPage from "../user-concerts-page/user-concerts-page.component";
 import UserConcertPage from "../user-concert-page/user-concert-page.component";
 import UserOrdersPage from "../user-orders-page/user-orders.component";
 import UserOrderPage from "../user-order-page/user-order.component";
+import UserInvitationsPage from "../user-invitations-page/user-invitations-page.component";
 
 import "./user-page.styles.scss";
 
 const UserPage = () => {
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
     dispatch(getUserStart());
@@ -48,16 +47,13 @@ const UserPage = () => {
               path="/user/concerts/:id"
               component={UserConcertPage}
             />
+            <Route exact path="/user/orders" component={UserOrdersPage} />
+            <Route exact path="/user/orders/:id" component={UserOrderPage} />
             <Route
               exact
               path="/user/invitations"
-              component={InvitationsTab}
-              email={user.email}
-              artistID={user.artistID}
-              bands={user.bands}
+              component={UserInvitationsPage}
             />
-            <Route exact path="/user/orders" component={UserOrdersPage} />
-            <Route exact path="/user/orders/:id" component={UserOrderPage} />
           </Switch>
         </div>
       </div>
