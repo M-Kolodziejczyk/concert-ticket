2b0d08f768fe5ae1a3f86a33c6b2a6e95f87e468
MK <kolodziejczykmichal269@gmail.com>
2021-06-26 11:27:26 +0200

    Create HomePage

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./home-page.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+        itaque, corrupti praesentium et quas laboriosam natus dignissimos,
+        aliquam vitae rem reiciendis delectus incidunt illum placeat, eos
+        consectetur quo enim aliquid. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Consequatur tenetur voluptates temporibus culpa
+        nostrum sunt rem iste, sed maiores, id cupiditate laudantium numquam ad,
+        assumenda nulla repellendus quam eos a! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Necessitatibus, quidem beatae! Nisi
+        repellat blanditiis rerum alias odio ut tenetur ad maxime earum
+        molestiae laudantium, laborum temporibus magni quas, optio dolor.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+        itaque, corrupti praesentium et quas laboriosam natus dignissimos,
+        aliquam vitae rem reiciendis delectus incidunt illum placeat, eos
+        consectetur quo enim aliquid. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Consequatur tenetur voluptates temporibus culpa
+        nostrum sunt rem iste, sed maiores, id cupiditate laudantium numquam ad,
+        assumenda nulla repellendus quam eos a! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Necessitatibus, quidem beatae! Nisi
+        repellat blanditiis rerum alias odio ut tenetur ad maxime earum
+        molestiae laudantium, laborum temporibus magni quas, optio dolor.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
