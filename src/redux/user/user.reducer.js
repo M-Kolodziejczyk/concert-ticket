da18f55397af1a04b4f909242b63e8a57aa1c342
MK <kolodziejczykmichal269@gmail.com>
2021-06-28 12:40:47 +0200

    Add formLoading in rducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -15,33 +15,60 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case UserActionTypes.SIGN_UP_START:
+      return {
+        ...state,
+        formLoading: true,
+      };
     case UserActionTypes.SIGN_UP_SUCCESS:
       return {
         ...state,
+        formLoading: false,
         pushRoute: "/",
         errorMessage: {},
-        successMessage: { signup: "You have signed up successfully" },
+        successMessage: {
+          signup:
+            "You have signed up successfully, check your email and go to confirm page",
+        },
       };
     case UserActionTypes.SIGN_UP_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: { signup: action.payload.message },
+      };
+    case UserActionTypes.SIGN_UP_CONFIRM_START:
+      return {
+        ...state,
+        formLoading: true,
       };
     case UserActionTypes.SIGN_UP_CONFIRM_SUCCESS:
       return {
         ...state,
-        successMessage: { confirmSignup: "Your registration is complete" },
+        formLoading: false,
+        successMessage: {
+          confirmSignup:
+            "Your registration has been successfully completed, you can now sign in",
+        },
         errorMessage: {},
       };
     case UserActionTypes.SIGN_UP_CONFIRM_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: { confirmSignup: action.payload.message },
         successMessage: {},
+      };
+    case UserActionTypes.GOOGLE_SIGN_IN_START:
+    case UserActionTypes.EMAIL_SIGN_IN_START:
+      return {
+        ...state,
+        formLoading: true,
       };
     case UserActionTypes.SIGN_IN_SUCCESS:
       return {
         ...state,
+        formLoading: false,
         pushRoute: "/",
         currentUser: action.payload,
         errorMessage: {},
@@ -52,25 +79,41 @@ const userReducer = (state = INITIAL_STATE, action) => {
     case UserActionTypes.SIGN_IN_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: { signin: action.payload.message },
         isLogged: false,
         userLoading: false,
       };
+    case UserActionTypes.RESEND_CODE_START:
+      return {
+        ...state,
+        formLoading: true,
+      };
     case UserActionTypes.RESEND_CODE_SUCCESS:
       return {
         ...state,
-        successMessage: { resendCode: action.payload },
+        formLoading: false,
+        successMessage: {
+          resendCode: "Confirm code sent successfuly, check your email.",
+        },
         errorMessage: {},
       };
     case UserActionTypes.RESEND_CODE_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: { resendCode: action.payload.message },
         successMessage: {},
+      };
+    case UserActionTypes.FORGOT_PASSWORD_START:
+      return {
+        ...state,
+        formLoading: true,
       };
     case UserActionTypes.FORGOT_PASSWORD_SUCCESS:
       return {
         ...state,
+        formLoading: false,
         successMessage: {
           forgotPassword: "We've sent a reset code to your email.",
         },
@@ -79,14 +122,21 @@ const userReducer = (state = INITIAL_STATE, action) => {
     case UserActionTypes.FORGOT_PASSWORD_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: {
           forgotPassword: action.payload.message,
         },
         successMessage: {},
       };
+    case UserActionTypes.NEW_PASSWORD_START:
+      return {
+        ...state,
+        formLoading: true,
+      };
     case UserActionTypes.NEW_PASSWORD_SUCCESS:
       return {
         ...state,
+        formLoading: false,
         successMessage: {
           newPassword: "Your password has been successfully changed.",
         },
@@ -95,14 +145,21 @@ const userReducer = (state = INITIAL_STATE, action) => {
     case UserActionTypes.NEW_PASSWORD_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: {
           newPassword: action.payload.message,
         },
         successMessage: {},
       };
+    case UserActionTypes.SIGN_OUT_START:
+      return {
+        ...state,
+        formLoading: true,
+      };
     case UserActionTypes.SIGN_OUT_SUCCESS:
       return {
         ...state,
+        formLoading: false,
         currentUser: null,
         errorMessage: {},
         successMessage: { signout: "Successfully signed out" },
@@ -112,6 +169,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
     case UserActionTypes.SIGN_OUT_FAILURE:
       return {
         ...state,
+        formLoading: false,
         errorMessage: { signout: action.payload.message },
         successMessage: {},
       };
