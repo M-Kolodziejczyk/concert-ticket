8e9d67d9d4831b63182e475f589e7d356c425fd5
MK <kolodziejczykmichal269@gmail.com>
2021-06-28 12:42:26 +0200

    Rebuild forgot-new-password-page and RWD

--- src/pages/forgot-new-password-page/forgot-new-password.component.jsx
+++ src/pages/forgot-new-password-page/forgot-new-password.component.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import ForgotPassword from "../../components/forgot-password/forgot-password.component";
 import NewPassword from "../../components/new-password/new-password.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import "./forgot-new-password.styles.scss";
 
 const ForgotNewPasswordPage = () => {
+  const userFormLoading = useSelector((state) => state.user.formLoading);
   return (
     <div className="forgot-new-password">
+      {userFormLoading && <Spinner />}
       <ForgotPassword />
       <NewPassword />
     </div>
