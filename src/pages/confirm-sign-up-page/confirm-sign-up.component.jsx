6a342bd0d3bec5d958910c1de56297b55f17b0ec
MK <kolodziejczykmichal269@gmail.com>
2021-06-28 12:41:38 +0200

    Rebuild confirm-signup-page and RWD

--- src/pages/confirm-sign-up-page/confirm-sign-up.component.jsx
+++ src/pages/confirm-sign-up-page/confirm-sign-up.component.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import ConfirmSignUp from "../../components/confirm-sign-up/confirm-sign-up.component";
 import ResendCode from "../../components/resend-code/resend-code.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import "./confirm-sign-up.styles.scss";
 
 const ConfirmSignUpPage = () => {
+  const userformLoading = useSelector((state) => state.user.formLoading);
+
   return (
     <div className="confirmPage">
+      {userformLoading && <Spinner />}
       <ConfirmSignUp />
       <ResendCode />
     </div>
