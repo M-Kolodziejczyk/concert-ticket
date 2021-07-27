15572e23e070ea6abea89df39d66011848a103f1
MK <kolodziejczykmichal269@gmail.com>
2021-07-27 14:49:31 +0200

    update privateRoute if staement

--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+import Spinner from "../spinner/spinner.component";
+
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const userLoading = useSelector((state) => state.user.userLoading);
   const isLogged = useSelector((state) => state.user.isLogged);
+  const isAccountDeleted = useSelector((state) => state.user.isAccountDeleted);
+
   if (userLoading) {
-    return <h1>LOADING</h1>; /// temporary!!
+    return <Spinner />;
   } else {
     if (isLogged === true) {
       return <Route {...rest} render={(props) => <Component {...props} />} />;
+    } else if (isLogged === false && isAccountDeleted === true) {
+      return <Redirect to="/user-deleted" />;
+    } else {
+      return <Redirect to="/signin" />;
     }
-    return <Redirect to="/signin" />;
   }
 };
 
