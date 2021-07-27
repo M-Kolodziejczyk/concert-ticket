ff9c521cbffcdfa2e0b84f491fb7d12a979c8fce
MK <kolodziejczykmichal269@gmail.com>
2021-07-27 15:18:37 +0200

    update user-details

--- src/pages/user-details-page/user-details-page.component.jsx
+++ src/pages/user-details-page/user-details-page.component.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { changePasswordStart } from "../../redux/user/user.actions";
-import validate from "../../validators/changePassword";
 
+import {
+  changePasswordStart,
+  deleteUserStart,
+} from "../../redux/user/user.actions";
+import validate from "../../validators/changePassword";
 import useForm from "../../hooks/useForm";
 
 import Spinner from "../../components/spinner/spinner.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import FormInput from "../../components/form-input/form-input.component";
+import DeleteModal from "../../components/delete-modal/delete-modal.component";
 
 import "./user-details-page.styles.scss";
 
 const UserDetailsPage = () => {
+  let history = useHistory();
   const formLoading = useSelector((state) => state.user.formLoading);
   const successMessage = useSelector((state) => state.user.successMessage);
   const errorMessage = useSelector((state) => state.user.errorMessage);
+  const user = useSelector((state) => state.user.user);
   const userEmail = useSelector((state) => state.user.user.email);
+  const isAccountDeleted = useSelector((state) => state.user.isAccountDeleted);
 
   const { handleChange, handleSubmit, values, errors } = useForm(
     { oldPassword: "", newPassword: "" },
@@ -23,9 +31,31 @@ const UserDetailsPage = () => {
     changePasswordStart
   );
 
+  useEffect(() => {
+    if (isAccountDeleted === true && !formLoading) {
+      history.push("/user-deleted");
+    }
+  }, [isAccountDeleted, history, formLoading]);
+
   return (
     <div className="user-details-page">
       {formLoading && <Spinner />}
+      <div className="delete-container">
+        <DeleteModal
+          title={`Are you sure you want to delete ${userEmail}`}
+          deleteBtn="Delete user"
+          deleteConfirm="Delete"
+          id={user.owner}
+          callback={deleteUserStart}
+          successMessage={successMessage.removeConcert}
+          errorMessage={errorMessage.removeConcert}
+        />
+        <div className="messages">
+          {errorMessage.deleteUser && (
+            <p className="error">{errorMessage.deleteUser}</p>
+          )}
+        </div>
+      </div>
       <h1>User details</h1>
       <div className="details">
         <span className="email">Email: {userEmail}</span>
